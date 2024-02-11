7c0ce5e9d7f590ada84c7295323022b891eb7a1c
omarsouidi4 <omar.souidi@epfedu.fr>
2024-02-11 13:04:20 +0100

    deuxieme commit avec firebase

--- dj.js
+++ dj.js
@@ -1,4 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 import { getFirestore, collection, query, orderBy, onSnapshot } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyC2uaxWDXMh2NqBuOfDtal-tGEuqEp_DYY",
+    authDomain: "karaoke-1f4e6.firebaseapp.com",
+    projectId: "karaoke-1f4e6",
+    storageBucket: "karaoke-1f4e6.appspot.com",
+    messagingSenderId: "732700787552",
+    appId: "1:732700787552:web:d2f21e6334de13cb959405",
+    measurementId: "G-564LSTD2Q4"
+  };
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 document.addEventListener('DOMContentLoaded', () => {
     const db = getFirestore(); // Obtenez une référence à la base de données Firestore
